57df9b5842752ea62721b9d0a84e49eee56cf0d7
AnqurVanillapy <anqurvanillapy@gmail.com>
2017-03-20 00:00:26 +0800

    updated tim.h

--- src/server.c
+++ src/server.c
@@ -3,6 +3,6 @@
 int
 main(int argc, const char *argv[])
 {
-    
+    if (argc != 2) err_quit("usage: tim-server ip_addr port");
     return 0;
 }
--- src/tim.h
+++ src/tim.h
@@ -1,16 +1,33 @@
+#ifndef __tim__tim_h
+#define __tim__tim_h
+
 #include <stdio.h>
 #include <semaphore.h>
 
 #define MSGSIZ  256 // max #bytse per message, including null at end
 #define NMSG    16  // max #messages
 
+/// Error handling.
+#define err_quit(errmsg) do {   \
+    fputs(errmsg, stdout);      \
+    exit(EXIT_FAILURE);         \
+} while(0)
+
+#define err_sys(errmsg) do {    \
+    perror(errmsg);             \
+    exit(EXIT_FAILURE);         \
+} while(0)
+
 /// This is stored in shared memory.
 struct tim_shm {
-    sem_t   mutex;          // Three memory-based semaphores for
-    sem_t   nempty;         // producer-consumer buffer.
+    sem_t   mutex;                  // Three memory-based semaphores for
+    sem_t   nempty;                 // producer-consumer buffer.
     sem_t   nstored;
-    int     nput;           // index of next message
-    long    noverflow;      // #overflows by the senders
-    sem_t   mutex_overflow; // mutex for noverflow counter
-    long    ;
-}
+    int     nput;                   // index of next message
+    long    noverflow;              // #overflows by the senders
+    sem_t   mutex_overflow;         // mutex for noverflow counter
+    long    msgoff[NMSG];           // offset of each message
+    char    msgdata[NMSG * MSGSIZ]; // actual messages
+};
+
+#endif // !__tim__tim_h
